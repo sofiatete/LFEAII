ffb0108f9c80aa2ac2827007d8d788e216bcbc08
pedrocurvo <95446270+pedrocurvo@users.noreply.github.com>
2023-10-11 11:27:55 +0100

    changes

--- source/main.py
+++ source/main.py
@@ -15,26 +15,28 @@ DATA_PATH.mkdir(exist_ok=True,
                     parents=True)
 
 for images in IMAGES_PATH.iterdir():
-    print(images.name)
-    # Load Image and convert to numpy array
-    IMAGE = images.name
-    img = Image.open(IMAGES_PATH/IMAGE)
-    img_arr = np.array(img)
-    print(f"Image shape: {img_arr.shape}")
+    if images.name == 'ab1.pgm':
+        print(images.name)
+        # Load Image and convert to numpy array
+        IMAGE = images.name
+        img = Image.open(IMAGES_PATH/IMAGE)
+        img_arr = np.array(img)
+        print(f"Image shape: {img_arr.shape}")
 
-    # Plot image
-    plt.imshow(img_arr, cmap='gray')
-    plt.axis('off')
-    plt.show()
+        # Plot image
+        plt.imshow(img_arr, cmap='gray')
+        plt.axis('off')
+        plt.show()
 
-    # Gaussian 3d from image 
-    gaussian_img = img_arr
-    gaussian_img_norm = (gaussian_img - np.min(gaussian_img)) / (np.max(gaussian_img) - np.min(gaussian_img))
-    fig = plt.figure(figsize=(10, 10))
-    ax = fig.add_subplot(111, projection='3d')
-    x, y = np.mgrid[:gaussian_img_norm.shape[0], :gaussian_img_norm.shape[1]]
-    ax.plot_surface(x, y, gaussian_img_norm, cmap='viridis')
-    plt.show()
+        # Gaussian 3d from image 
+        gaussian_img = img_arr
+        gaussian_img_norm = (gaussian_img - np.min(gaussian_img)) / (np.max(gaussian_img) - np.min(gaussian_img))
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.add_subplot(111, projection='3d')
+        x, y = np.mgrid[:gaussian_img_norm.shape[0], :gaussian_img_norm.shape[1]]
+        ax.plot_surface(x, y, gaussian_img_norm, cmap='viridis')
+        plt.show()
+
 
 
 
